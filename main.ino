a475479e11d6d36865c6ecb0a44cf354229ccfca
masha <57405750+Massshhhaaa@users.noreply.github.com>
2020-03-04 22:11:28 +0300

    Update main.ino

--- main.ino
+++ main.ino
@@ -26,8 +26,9 @@ void setup(){
   radio.setPALevel (RF24_PA_MAX); //уровень мощности передатчика. На выбор RF24_PA_MIN, RF24_PA_LOW, RF24_PA_HIGH, RF24_PA_MAX
   radio.setDataRate (RF24_1MBPS);  //скорость обмена. На выбор RF24_2MBPS, RF24_1MBPS, RF24_250KBPS
   radio.setAutoAck(1);         //режим подтверждения приёма, 1 вкл 0 выкл
-  radio.setRetries(2, 2);    //(время между попыткой достучаться, число попыток)
-  radio.enableAckPayload();    //разрешить отсылку данных в ответ на входящий сигнал
+  radio.setRetries(0, 15);    //(время между попыткой достучаться, число попыток)
+  radio.enableAckPayload();
+  radio.enableDynamicAck();     //разрешить отсылку данных в ответ на входящий сигнал
   radio.setPayloadSize(32);
   radio.powerUp();
 
@@ -42,20 +43,20 @@ void identification(){
   //read eeprom memory for identification and ID sending delay
   int id;
   int queue_delay;
-
+  radio.openWritingPipe(address[1]);
   radio.openReadingPipe(1, address[1]);
   radio.startListening();
-  EEPROM.get(0, id); EEPROM.commit(); EEPROM.get(2; queue_delay); EEPROM.commit();
+  EEPROM.get(0, id);
+  EEPROM.get(5; queue_delay);
+  EEPROM.commit();
 
      while (true){
-
+       delay(1);
         // branch for passive participants
        if (radio.available()){
          radio.read(&incoming_identification_request, sizeof(incoming_identification_request));
          if (incoming_identification_request == 1111){
            Serial.println("этот пидорас получил запрос на идентификацию");
-           radio.closeReadingPipe(1);
-           radio.openWritingPipe(address[2]);//актуально будет поменять трубу для телеметрии
            radio.stopListening();
            delay(queue_delay);
            radio.write(&id, sizeof(id));
@@ -66,9 +67,7 @@ void identification(){
 
         // branch for beginner
         if ((Serial.available() > 0) && (Serial.read() == 'S')){
-        // identification_procedure_start = true;
          radio.stopListening();
-         radio.openWritingPipe(address[1]);
          radio.write(&request_for_identification, sizeof(request_for_identification), true);
          Serial.println("sended request for ident");
 
@@ -76,6 +75,7 @@ void identification(){
          for (int i = 0; i < 2; i++) {
             Serial.println(identificators_list[i]);
           }
+          break;
 
      }
 }}
@@ -84,10 +84,11 @@ void identification(){
 void ident_telemetry_acceptance(bool ident_telemetry_accep){
   uint8_t time = millis();
   int i = 0;
-  radio.openReadingPipe(1, address[2]);
+  radio.openReadingPipe(1, address[1]);
   radio.startListening();
 
   while((ident_telemetry_accep) && (millis() - time < 5000)){
+    delay(1);
     if (radio.available()){
       radio.read(&incoming_id, sizeof(incoming_id));
       Serial.println("id получен, придурок");
